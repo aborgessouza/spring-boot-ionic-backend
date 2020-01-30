6caa1ef0258940696f8504f497130cbe6c558f28
Alexandre Borges Souza <aborgessouza@gmail.com>
2020-01-29 21:36:20 -0300

    Produto associacao muitos para muitos

---
+++ src/main/java/com/alexandresouza/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.alexandresouza.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandresouza.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Integer>{
+
+}
